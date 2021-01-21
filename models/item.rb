0aa15d57270888fe60cff11a47ea1bc71af693c1
rsneal01 <rsneal01@gmail.com>
2021-01-21 11:05:45 -0800

    commit tree 6d123bd5d589741745352d6b62f6f7f7bec7cafb

---
+++ models/item.rb
@@ -0,0 +1,18 @@
+# class Item
+#     attr_accessor :name
+
+#     @@all = []
+
+#     def initialize(params)
+#         @name = params[:name]
+#     end
+
+#     def self.all
+#         @@all
+#       end
+      
+#       def self.clear
+#         @@all.clear
+#       end
+
+# end
